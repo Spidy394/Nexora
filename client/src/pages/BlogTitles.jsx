014d403843250005556e8fdfa6f244a6784ca41c
Spidy394 <79023855+Spidy394@users.noreply.github.com.>
2025-07-17 19:20:31 +0530

    finish image gen

--- client/src/pages/BlogTitles.jsx
+++ client/src/pages/BlogTitles.jsx
@@ -1,4 +1,4 @@
-import { Edit, Hash, Sparkles } from "lucide-react";
+import { Hash, Sparkles } from "lucide-react";
 import React, { useState } from "react";
 
 const BlogTitles = () => {
@@ -76,7 +76,7 @@ const BlogTitles = () => {
         <div className="flex-1 flex justify-center items-center">
           <div className="text-sm flex flex-col items-center gap-5 text-gray-400">
             <Hash className="w-9 h-9" />
-            <p>Enter a topic and click "Generate title" to get started...</p>
+            <p>Enter a topic and click "Generate tile" to get started...</p>
           </div>
         </div>
       </div>
